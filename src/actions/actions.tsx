210e00e1ff416941120df6e7888ef8dba8809de6
vijay <nimmagaddav9@gmail.com>
2020-12-04 18:43:28 -0800

    actions code

--- src/actions/actions.tsx
+++ src/actions/actions.tsx
@@ -25,6 +25,54 @@ export const employees = (records: any) => {
 /***************************Add Employee***********************************/
 //record coming from component
 export const addEmployee = (record: any) => {
-  return (dispatch: any) => {};
+  return (dispatch: any) => {
+    return axios.post("http://localhost:8080/insert", record).then(
+      (posRes) => {
+        dispatch(fun_two(posRes.data));
+      },
+      (errRes: any) => {
+        console.log(errRes);
+      }
+    );
+  };
 };
+export const fun_two = (status: any) => {
+  return { type: "INSERT", value: status };
+};
+
 /*************************************************************************/
+
+/***************************************UPDATE*******************************/
+export const updateEmployee = (record: any) => {
+  return (dispatch: any) => {
+    return axios.put("http://localhost:8080/update", record).then(
+      (posRes) => {
+        dispatch(fun_three(posRes.data));
+      },
+      (errRes: any) => {
+        console.log(errRes);
+      }
+    );
+  };
+};
+export const fun_three = (status: any) => {
+  return { type: "UPDATE", value: status };
+};
+/****************************************************************************/
+/************************************DELETE**********************************/
+export const deleteEmployee = (record: any) => {
+  return (dispatch: any) => {
+    return axios.delete("http://localhost:8080/delete", { data: record }).then(
+      (posRes) => {
+        dispatch(fun_four(posRes.data));
+      },
+      (errRes: any) => {
+        console.log(errRes);
+      }
+    );
+  };
+};
+export const fun_four = (status: any) => {
+  return { type: "DELETE", value: status };
+};
+/****************************************************************************/
